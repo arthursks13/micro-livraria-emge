93dcc8070de7901e9d2cc86eb37fe8597159f3f1
Arthur Sanches <66748313+arthursks13@users.noreply.github.com>
2021-06-16 15:33:36 -0300

    Tarefa Prática EMGE-CCES#3: Implementar o controle de estoque dos livros

--- services/inventory/index.js
+++ services/inventory/index.js
@@ -2,6 +2,8 @@ const grpc = require('@grpc/grpc-js');
 const protoLoader = require('@grpc/proto-loader');
 const products = require('./products.json');
 
+const productsRepository = JSON.parse(JSON.stringify(products));
+
 const packageDefinition = protoLoader.loadSync('proto/inventory.proto', {
     keepCase: true,
     longs: String,
@@ -24,6 +26,28 @@ server.addService(inventoryProto.InventoryService.service, {
         callback(
             null,
             products.find((product) => product.id == payload.request.id)
+        );
+    },
+    AddProduct: (payload, callback) => {
+        let id = 1;
+        for (const item of products){
+            if (item.id >= id) {
+                id = item.id + 1;
+            }
+        }
+        const product = payload.request;
+        product.id = id;
+        productsRepository.push(product);
+        console.log(product);
+        callback(null, product);
+    },
+    UpdateInventory: (payload, callback) => {
+        let product = products.find((product) => product.id == payload.request.id)
+        product.quantity--;
+        console.log(product);
+        callback(
+            null,
+            product
         );
     },
 });
